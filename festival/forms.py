9c8c5234016cb9e61ba9fa7d43f42193d68f2671
eloi.grau@gmail.com <eloi.grau@gmail.com>
2019-12-02 18:22:46 +0100

    init par ecocharte

---
+++ festival/forms.py
@@ -0,0 +1,135 @@
+from django import forms
+from django.core.validators import RegexValidator
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import Adresse, Profil, Message, Choix, Commentaire_charte
+from captcha.fields import CaptchaField
+from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
+
+no_space_validator = RegexValidator(
+      r' ',
+      ("Le pseudonyme ne doit pas contenir d'espaces"),
+      inverse_match=True,
+      code='invalid_tag',
+  )
+
+class AdresseForm(forms.ModelForm):
+    code_postal = forms.CharField(label="Code postal*", )
+    commune = forms.CharField(label="Commune*", )
+    latitude = forms.FloatField(label="Latitude", initial="42", required=False,widget = forms.HiddenInput())
+    longitude = forms.FloatField(label="Longitude", initial="2", required=False,widget = forms.HiddenInput())
+
+    class Meta:
+        model = Adresse
+        exclude = ('latitude', 'longitude')
+
+    def save(self, *args, **kwargs):
+        adresse = super(AdresseForm, self).save(commit=False)
+        adresse.set_latlon_from_adresse()
+        adresse.save()
+        return adresse
+
+class ProfilCreationForm(UserCreationForm):
+    username = forms.CharField(label="Pseudonyme*", help_text="Attention les majuscules sont importantes...", validators=[no_space_validator,])
+    description = forms.CharField(label=None, help_text="Une description de vous même", required=False, widget=forms.Textarea)
+    captcha = CaptchaField()
+    email= forms.EmailField(label="Email*",)
+    accepter_annuaire = forms.BooleanField(required=False, label="J'accepte d'apparaitre dans l'annuaire du site et la carte et rend mon profil visible par tous les inscrits")
+    accepter_conditions = forms.BooleanField(required=True, label="J'ai lu et j'accepte les Conditions Générales d'Utilisation du site",  )
+
+    class Meta(UserCreationForm):
+        model = Profil
+        fields = ['username', 'password1',  'password2', 'first_name', 'last_name', 'email',  'description', 'inscrit_newsletter', 'accepter_annuaire', 'accepter_conditions']
+        exclude = ['slug', ]
+
+    def save(self, commit = True, is_active=False):
+        return super(ProfilCreationForm, self).save(commit)
+        self.is_active=is_active
+
+
+
+class ProfilChangeForm(UserChangeForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+    email = forms.EmailField(label="Email")
+    username = forms.CharField(label="Pseudonyme", validators=[no_space_validator,])
+    description = forms.CharField(label="Description", help_text="Une description de vous-même", required=False)
+    inscrit_newsletter = forms.BooleanField(required=False, label="J'accepte de recevoir des emails")
+    accepter_annuaire = forms.BooleanField(required=False, label="J'accepte d'apparaitre dans l'annuaire du site et la carte et rend mon profil visible par tous")
+    password=None
+
+    class Meta:
+        model = Profil
+        fields = ['username', 'first_name', 'last_name', 'email',  'description', 'accepter_annuaire', 'inscrit_newsletter', 'a_signe']
+
+
+class ProfilChangeForm_admin(UserChangeForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+    email = forms.EmailField(label="Email")
+    username = forms.CharField(label="Pseudonyme", validators=[no_space_validator,])
+    description = forms.CharField(label="Description", initial="Une description de vous même (facultatif)", widget=forms.Textarea)
+    inscrit_newsletter = forms.BooleanField(required=False)
+    accepter_annuaire = forms.BooleanField(required=False)
+    a_signe = forms.BooleanField(required=False)
+    password = None
+
+    class Meta:
+        model = Profil
+        fields = ['username', 'email', 'description', 'inscrit_newsletter', 'accepter_annuaire', ]
+
+    def __init__(self, *args, **kwargs):
+        super(ProfilChangeForm_admin, self).__init__(*args, **kwargs)
+
+
+class ContactForm(forms.Form):
+    sujet = forms.CharField(max_length=100, label="Sujet",)
+    msg = forms.CharField(label="Message", widget=forms.Textarea)
+    renvoi = forms.BooleanField(label="recevoir une copie",
+                                     help_text="Cochez si vous souhaitez obtenir une copie du mail envoyé.", required=False
+                                 )
+
+
+class MessageForm(forms.ModelForm):
+    message = forms.CharField(max_length=100, label="Laisser un commentaire...",)
+
+    class Meta:
+        model = Message
+        exclude = ['auteur', 'date_creation', 'type_article', 'type_message', 'valide']
+
+        widgets = {
+                'message': forms.Textarea(attrs={'rows': 2}),
+            }
+
+    def __init__(self, request, message=None, *args, **kwargs):
+        super(MessageForm, self).__init__(request, *args, **kwargs)
+        if message:
+           self.fields['message'].initial = message
+
+
+
+class CommentaireForm(forms.ModelForm):
+    message = forms.CharField(max_length=500, label="Laisser un commentaire...",)
+
+    class Meta:
+        model = Commentaire_charte
+        exclude = ['auteur', 'date_creation', 'type_message', 'valide', 'proposition']
+
+        widgets = {
+                'message': forms.Textarea(attrs={'rows': 2}),
+            }
+
+    def __init__(self, request, message=None, *args, **kwargs):
+        super(CommentaireForm, self).__init__(request, *args, **kwargs)
+        if message:
+           self.fields['message'].initial = message
+
+
+class SignerForm(forms.Form):
+    renvoi = forms.BooleanField(label="J'ai lu la charte", required=True
+                                 )
+    accepter = forms.BooleanField(label="J'accepte les termes de la charte, et je m'engage à les appliquer", required=True
+                                 )
---
+++ festival/urls.py
@@ -0,0 +1,88 @@
+"""festival URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.urls import path
+from festival import views
+from django.views.generic import TemplateView
+#from fcm_django.api.rest_framework import FCMDeviceAuthorizedViewSet
+
+# On import les vues de Django, avec un nom spécifique
+from django.contrib.auth.decorators import login_required
+
+# admin.autodiscover()
+from django.contrib import admin
+
+
+admin.sites.site_header ="Admin "
+admin.sites.site_title ="Admin Permacat"
+
+
+urlpatterns = [
+    path('gestion/', admin.site.urls),
+    url(r'^summernote/', include('django_summernote.urls')),
+    url(r'^captcha/', include('captcha.urls')),
+    url('^', include('django.contrib.auth.urls')),
+    url(r'^$', views.bienvenue, name='bienvenue'),
+    url(r'^risques/$', views.risques, name='risques'),
+    url(r'^introduction/$', views.introduction, name='introduction'),
+    url(r'^preconisations/$', views.preconisations, name='preconisations'),
+    url(r'^faq/$', views.faq, name='faq'),
+    url(r'^contact/$', views.contact, name='contact', ),
+    url(r'^signer/$', views.signer, name='signer', ),
+    url(r'^statistiques/$', views.statistiques, name='statistiques', ),
+    url(r'^signataires/$', views.signataires, name='signataires', ),
+
+    url(r'^accounts/profil/(?P<user_id>[0-9]+)/$', login_required(views.profil), name='profil', ),
+    url(r'^accounts/profil/(?P<user_username>[-\w.]+)/$', login_required(views.profil_nom), name='profil_nom', ),
+    url(r'^accounts/profile/$', login_required(views.profil_courant), name='profil_courant', ),
+    url(r'^accounts/profil_modifier/$', login_required(views.profil_modifier.as_view()), name='profil_modifier',),
+    url(r'^accounts/profil_supprimer/$', login_required(views.profil_supprimer.as_view()), name='profil_supprimer',),
+    url(r'^accounts/profil_modifier_adresse/$', login_required(views.profil_modifier_adresse.as_view()), name='profil_modifier_adresse',),
+
+    url(r'^merci/$', views.merci, name='merci'),
+    url(r'^register/$', views.register, name='senregistrer', ),
+    url(r'^password/change/$', views.change_password, name='change_password'),
+    path('auth/', include('django.contrib.auth.urls')),
+    url(r'^charte/$', views.charte, name='charte', ),
+    url(r'^cgu/$', views.cgu, name='cgu', ),
+    url(r'^liens/$', views.liens, name='liens', ),
+    url(r'^fairedon/$', views.fairedon, name='fairedon', ),
+    url(r'^contact_admins/$', views.contact_admins, name='contact_admins',),
+
+    url(r'^ajouterPointsCharte/$', views.ajouterPointsCharte, name='ajouterPointsCharte', ),
+    url(r'^voirPropositionCharte/(?P<slug>[-\w]+)$', views.voirPropositionCharte, name='voirPropositionCharte', ),
+    url(r'^ajouterVote_plus/(?P<slug>[-\w]+)$', views.ajouterVote_plus, name='ajouterVote_plus', ),
+    url(r'^ajouterVote_moins/(?P<slug>[-\w]+)$', views.ajouterVote_moins, name='ajouterVote_moins', ),
+
+]
+urlpatterns += [
+    url(r'^robots\.txt$', TemplateView.as_view(template_name="festival/robots.txt", content_type='text/plain')),
+]
+
+from django.conf import settings
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'festival.views.handler404'
+handler500 = 'festival.views.handler500'
+handler400 = 'festival.views.handler400'
+handler403 = 'festival.views.handler403'
+
+if settings.LOCALL:
+    import debug_toolbar
+    urlpatterns = [url(r'^__debug__/', include(debug_toolbar.urls)),] + urlpatterns
+    #urlpatterns += url('',(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}))
