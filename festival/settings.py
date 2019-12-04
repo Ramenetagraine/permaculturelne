97eb93492914b2c6bf5c8a8046ba3a9a2f3379f0
eloi.grau@gmail.com <eloi.grau@gmail.com>
2019-12-04 20:42:23 +0100

    icones

--- festival/settings.py
+++ festival/settings.py
@@ -71,7 +71,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -236,11 +236,13 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.normpath(os.path.join(BASE_DIR, '/media/'))
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # LOCATION_FIELD = {
 #     'map.provider': 'openstreetmap',
 # }
+
 
 
 LEAFLET_CONFIG = {
