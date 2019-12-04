b74124ed552f8b1b645a5b194f938f53498f4681
eloi.grau@gmail.com <eloi.grau@gmail.com>
2019-12-04 20:48:07 +0100

    icones

--- festival/wsgi.py
+++ festival/wsgi.py
@@ -13,6 +13,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 from festival import settings
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "festival.settings")
 
 application = get_wsgi_application()
